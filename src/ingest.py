77a53e25e5c95d6418581db945c0f83b13e1de52
Niels Wouda <niels.wouda@gettonline.com>
2022-04-28 16:32:13 +0200

    Steps to take for ingesting data

--- src/ingest.py
+++ src/ingest.py
@@ -2,9 +2,14 @@ from pathlib import Path
 
 
 def main():
+    # Remove existing database, if any - ingest starts from scratch.
     db = Path("data/waste.db")
     db.unlink(missing_ok=True)
 
+    # Set-up database connection and table structure.
+    # TODO
+
+    # Import data from raw Excel files.
     # TODO
 
 
