8d8f811f30f3143d9fdbdfcaa9a726aa25221444
Niels Wouda <nielswouda@gmail.com>
2023-04-17 12:14:35 +0200

    Use contraction hierarchies

--- osrm/run.bat
+++ osrm/run.bat
@@ -7,8 +7,7 @@ set mounts=-v "%curdir%/data:/data" -v "%curdir%/profiles/car.lua:/opt/car.lua" 
 :: Redo preparation work because the profile could have changed. It's pretty fast on most machines,
 :: so there's no great pain in doing this every time.
 docker run -t %mounts% ghcr.io/project-osrm/osrm-backend osrm-extract -p /opt/car.lua %where%.osm.pbf
-docker run -t %mounts% ghcr.io/project-osrm/osrm-backend osrm-partition %where%.osrm
-docker run -t %mounts% ghcr.io/project-osrm/osrm-backend osrm-customize %where%.osrm
+docker run -t %mounts% ghcr.io/project-osrm/osrm-backend osrm-contract %where%.osrm
 
 :: Runs the actual OSRM backend on port 5000.
-docker run -t -i -p 5000:5000 -v "%curdir%/data:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm mld %where%.osrm
+docker run -t -i -p 5000:5000 -v "%curdir%/data:/data" ghcr.io/project-osrm/osrm-backend osrm-routed --algorithm ch %where%.osrm
