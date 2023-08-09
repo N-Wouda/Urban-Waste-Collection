81dbd0ddc30e3f680239eef4b12c76ca64893387
Nicky van Foreest <vanforeest@gmail.com>
2023-08-09 10:24:03 +0200

    service events included

--- tests/classes/test_Simulator.py
+++ tests/classes/test_Simulator.py
@@ -6,31 +6,6 @@ from waste.constants import HOURS_IN_DAY
 
 
 def test_events_are_sealed_and_stored_property():
-    container = Container("test", [1] * HOURS_IN_DAY, 1.0, (0.0, 0.0))
-    sim = Simulator([], [], [container], [])
-
-    # Create some initial events for the simulator. After creation, new events
-    # are not yet sealed.
-    init = [ArrivalEvent(time, container, 0) for time in range(5)]
-    for event in init:
-        assert_(event.is_pending())
-        assert_(not event.is_sealed())
-
-    # Simulate and 'store' the sealed events to a list.
-    stored = []
-    sim(lambda event: stored.append(event), NullStrategy(), init)
-
-    # Simulation should not have created any new events.
-    assert_equal(len(init), len(stored))
-
-    # All events should now be sealed, and no longer pending.
-    for init_event, stored_event in zip(init, stored):
-        assert_(init_event is stored_event)
-        assert_(not init_event.is_pending())
-        assert_(init_event.is_sealed())
-
-
-def test_serviceevents_are_sealed_and_stored_properly():
     container = Container(
         name="test",
         rates=[1] * HOURS_IN_DAY,
@@ -41,11 +16,12 @@ def test_serviceevents_are_sealed_and_stored_properly():
         distances=[], durations=[], containers=[container], vehicles=[]
     )
 
-    # Create some initial serviceevents for the simulator. After creation,
-    # new events  are not yet sealed.
-    init = [
+    # Create some initial events for the simulator. After creation, new events
+    # are not yet sealed.
+    init = [ArrivalEvent(time, container, 0) for time in range(5)]
+    init += [
         ServiceEvent(time, id_route=1, container=container, vehicle=1)
-        for time in range(5)
+        for time in range(5, 10)
     ]
     for event in init:
         assert_(event.is_pending())
