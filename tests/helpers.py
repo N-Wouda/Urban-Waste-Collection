327634169a853eabadd83c0d154a525b0b6bf5b8
Niels Wouda <nielswouda@gmail.com>
2023-08-09 17:21:49 +0200

    Simplify typing

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,9 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from waste.classes.Event import ShiftPlanEvent as ShiftPlan
-    from waste.classes.Route import Route
-    from waste.classes.Simulator import Simulator
+    from waste.classes import Route, ShiftPlanEvent, Simulator
 
 
 class NullStrategy:
@@ -13,5 +11,5 @@ class NullStrategy:
     Strategy that does nothing.
     """
 
-    def __call__(self, sim: Simulator, event: ShiftPlan) -> list[Route]:
+    def __call__(self, sim: Simulator, event: ShiftPlanEvent) -> list[Route]:
         return []
