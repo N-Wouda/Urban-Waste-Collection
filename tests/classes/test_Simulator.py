15b408d65fe8fa212c14b48eaf9e9cf62a20c1ff
Nicky van Foreest <vanforeest@gmail.com>
2023-08-10 14:31:09 +0200

    tests repaired

--- tests/classes/test_Simulator.py
+++ tests/classes/test_Simulator.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from numpy.random import default_rng
 from numpy.testing import assert_, assert_equal
 
@@ -15,21 +17,35 @@ from waste.constants import HOURS_IN_DAY
 def test_events_are_sealed_and_stored_property():
     container = Container("test", [1] * HOURS_IN_DAY, 1.0, (0.0, 0.0))
     sim = Simulator(default_rng(0), [], [], [container], [])
+    from_time = datetime(2023, 8, 9, 10, 0)
+    until_time = datetime(2023, 8, 9, 12, 0)
 
     # Create some initial events for the simulator. After creation, new events
     # are not yet sealed.
     init = [
-        ArrivalEvent(time=0, container=container, volume=0),
-        ServiceEvent(time=1, id_route=1, container=container, vehicle=1),
-        ShiftPlanEvent(time=2),
+        ArrivalEvent(time=from_time, container=container, volume=0),
+        ServiceEvent(
+            time=from_time + timedelta(hours=1),
+            id_route=1,
+            container=container,
+            vehicle=1,
+        ),
+        ShiftPlanEvent(time=from_time + timedelta(hours=2)),
     ]
     for event in init:
         assert_(event.is_pending())
         assert_(not event.is_sealed())
 
     # Simulate and 'store' the sealed events to a list.
+
     stored = []
-    sim(lambda event: stored.append(event), NullStrategy(), init)
+    sim(
+        lambda event: stored.append(event),
+        NullStrategy(),
+        init,
+        from_time,
+        until_time,
+    )
 
     # Simulation should not have created any new events.
     assert_equal(len(init), len(stored))
@@ -42,9 +58,20 @@ def test_events_are_sealed_and_stored_property():
 
 
 def test_stored_events_are_sorted_in_time():
+    from_time = datetime(2023, 8, 9, 10, 0)
+    until_time = datetime(2023, 8, 9, 12, 0)
     sim = Simulator(default_rng(0), [], [], [], [])
-    init = [ShiftPlanEvent(time=time) for time in range(5, 0, -1)]
+    init = [
+        ShiftPlanEvent(time=from_time + timedelta(hours=hour))
+        for hour in range(5, 0, -1)
+    ]
 
     stored = []
-    sim(lambda event: stored.append(event), NullStrategy(), init)
+    sim(
+        lambda event: stored.append(event),
+        NullStrategy(),
+        init,
+        from_time,
+        until_time,
+    )
     assert_equal(stored, sorted(stored, key=lambda event: event.time))
