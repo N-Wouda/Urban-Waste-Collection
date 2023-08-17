272a6890d666f358281c21bb83bfa32cc8cbe8b0
Nicky van Foreest <vanforeest@gmail.com>
2023-08-17 09:38:53 +0200

    average route stops

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from waste.classes import Route
+
 if TYPE_CHECKING:
-    from waste.classes import Route, ShiftPlanEvent, Simulator
+    from waste.classes import ShiftPlanEvent, Simulator
 
 
 class NullStrategy:
@@ -16,3 +18,20 @@ class NullStrategy:
 
     def __call__(self, sim: Simulator, event: ShiftPlanEvent) -> list[Route]:
         return []
+
+
+class PeriodicStrategy:
+    """
+    Strategy that generates routes in a deterministic, period, way. Vehicle i
+    serves containers i, i+l, i+2l, ... where l is the number of vehicles.
+    """
+
+    def __init__(self, **kwargs):
+        pass
+
+    def __call__(self, sim: Simulator, event: ShiftPlanEvent) -> list[Route]:
+        routes = []
+        for i, vehicle in enumerate(sim.vehicles):
+            plan = list(range(i, len(sim.containers), len(sim.vehicles)))
+            routes.append(Route(plan, vehicle))
+        return routes
---
+++ tests/measures/test_avg_route_stops.py
@@ -0,0 +1,62 @@
+from datetime import date, datetime, time, timedelta
+
+import numpy as np
+import pandas as pd
+from numpy.random import default_rng
+from numpy.testing import assert_equal
+
+from tests.helpers import PeriodicStrategy
+from waste.classes import (
+    ArrivalEvent,
+    Container,
+    Database,
+    ShiftPlanEvent,
+    Simulator,
+    Vehicle,
+)
+from waste.constants import HOURS_IN_DAY, SHIFT_PLAN_TIME
+from waste.measures import avg_route_stops
+
+
+def test_num_arrivals():
+    num_vehicles = 3
+    num_containers = 20
+    vehicles = [Vehicle(f"a{i}", 4000.0) for i in range(num_vehicles)]
+    containers = [
+        Container(f"t{i}", [1] * HOURS_IN_DAY, 1000.0, (0, 0))
+        for i in range(num_containers)
+    ]
+
+    durations = np.ones(
+        (num_containers, num_containers), np.timedelta64(1, "s")
+    )
+    np.fill_diagonal(durations, 0)
+
+    strategy = PeriodicStrategy()
+    # The PeriodicStrategy distributes the containers evenly over the vehicles.
+    avg_stops_per_route = num_containers / num_vehicles
+
+    sim = Simulator(default_rng(0), [], durations, containers, vehicles)
+
+    src_db = res_db = ":memory:"
+    db = Database(src_db, res_db)
+
+    today = date.today()
+    start = datetime.combine(today, time.min)
+    end = datetime.combine(today, time.max) + timedelta(days=1)
+    freq = "2H"  # frequency of deposits
+
+    events = []
+    for container in containers:
+        deposit_times = pd.date_range(start, end, freq=freq).to_pydatetime()
+        volumes = [10] * len(deposit_times)
+        for t, volume in zip(deposit_times, volumes):
+            events.append(ArrivalEvent(t, container=container, volume=volume))
+
+    first_shift = datetime.combine(start, SHIFT_PLAN_TIME)
+    for t in pd.date_range(first_shift, end, freq="D").to_pydatetime():
+        events.append(ShiftPlanEvent(t))
+
+    sim(db.store, strategy, events)
+
+    assert_equal(db.compute(avg_route_stops), avg_stops_per_route)
