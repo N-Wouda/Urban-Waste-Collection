c3c3bc120218fbf93279acf18e114fc22025c4cb
Niels Wouda <nielswouda@gmail.com>
2023-08-18 15:00:27 +0200

    Update break start times

--- waste/constants.py
+++ waste/constants.py
@@ -24,9 +24,9 @@ SHIFT_DURATION: timedelta = timedelta(hours=8)
 VOLUME_RANGE: tuple[float, float] = (30, 65)  # in liters
 SHIFT_PLAN_TIME: time = time(hour=7)
 BREAKS: list[tuple[time, time, timedelta]] = [
-    # Coffee break starting between 10:00 and 10:15, lasting 15 minutes, and
-    # a lunch break starting between 12:00 and 12:30, lasting 30 minutes.
-    (time(hour=10), time(hour=10, minute=15), timedelta(minutes=15)),
-    (time(hour=12), time(hour=12, minute=30), timedelta(minutes=30)),
+    # Coffee break starting just after 10:00 and lasting 15 minutes, and
+    # a lunch break starting just after 12:00, lasting 30 minutes.
+    (time(hour=10), time(hour=10, minute=10), timedelta(minutes=15)),
+    (time(hour=12), time(hour=12, minute=15), timedelta(minutes=30)),
 ]
 TIME_PER_CONTAINER = timedelta(minutes=3)
