03b3fbd40f88dda5d9725e7e8b56757c15204b07
Niels Wouda <nielswouda@gmail.com>
2023-08-19 12:51:32 +0200

    Implement baseline

--- waste/strategies/BaselineStrategy.py
+++ waste/strategies/BaselineStrategy.py
@@ -31,11 +31,24 @@ class BaselineStrategy(GreedyStrategy):
     ):
         super().__init__(num_containers, max_runtime, **kwargs)
 
-        if deposit_volume < 0.0:
-            raise ValueError("Expected deposit_volume >= 0.")
+        if deposit_volume <= 0.0:
+            raise ValueError("Expected deposit_volume > 0.")
 
         self.deposit_volume = deposit_volume
 
     def _get_container_idcs(self, sim: Simulator) -> np.ndarray[int]:
-        # TODO
-        return np.array([])
+        # Step 1. Determine current volume in each container based on the
+        # current number of arrivals.
+        arrivals = np.array([c.num_arrivals for c in sim.containers])
+        curr_vol = self.deposit_volume * arrivals
+
+        # Step 2. Determine the amount of time it'll take for each container to
+        # fill up, given the current volume and the average arrival rate.
+        capacities = np.array([c.capacity for c in sim.containers])
+        max_extra = np.maximum(capacities - curr_vol, 0) / self.deposit_volume
+        avg_rates = np.array([np.mean(c.rates) for c in sim.containers])
+        num_hours = max_extra / avg_rates
+
+        # Step 3. Select the ``num_containers`` that will fill up the soonest.
+        top_k = np.argpartition(num_hours, self.num_containers)
+        return top_k[: self.num_containers]
