59df1675b4738bdcdb7ff9beb4874d6a0794ec27
Niels Wouda <nielswouda@gmail.com>
2023-08-20 21:07:15 +0200

    Update configuration

--- waste/classes/Configuration.py
+++ waste/classes/Configuration.py
@@ -5,12 +5,12 @@ from datetime import time, timedelta
 @dataclass
 class Configuration:
     BREAKS: tuple[tuple[time, time, timedelta], ...] = (
-        # Coffee break starting just after 10:00 and lasting 15 minutes, and
-        # a lunch break starting just after 12:00, lasting 30 minutes.
-        (time(hour=10), time(hour=10, minute=10), timedelta(minutes=15)),
-        (time(hour=12), time(hour=12, minute=15), timedelta(minutes=30)),
+        # Coffee break starting just after 10:00 and lasting 20 minutes, and
+        # a lunch break starting just after 12:00, lasting 45 minutes.
+        (time(hour=10), time(hour=10, minute=10), timedelta(minutes=20)),
+        (time(hour=12), time(hour=12, minute=15), timedelta(minutes=45)),
     )
-    SHIFT_DURATION: timedelta = timedelta(hours=8)
+    SHIFT_DURATION: timedelta = timedelta(hours=7)  # excl. breaks
     SHIFT_PLAN_TIME: time = time(hour=7)  # noqa: RUF009
     TIME_PER_CONTAINER: timedelta = timedelta(minutes=3)
     VOLUME_RANGE: tuple[float, float] = (30, 65)  # in liters
