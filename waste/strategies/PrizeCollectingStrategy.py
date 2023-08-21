783e34cc0aa9e85cba3ba4fdb01a4eb6f20d3a23
Niels Wouda <nielswouda@gmail.com>
2023-08-21 14:09:27 +0200

    Add sklearn, observe events, set-up initial models

--- waste/strategies/PrizeCollectingStrategy.py
+++ waste/strategies/PrizeCollectingStrategy.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+
+from sklearn.linear_model import SGDClassifier
+
 from waste.classes import Event, Route, ServiceEvent, ShiftPlanEvent, Simulator
 
 
@@ -6,21 +10,38 @@ class PrizeCollectingStrategy:
     Dispatching via prize-collecting.
     """
 
-    def __init__(self, **kwargs):
-        pass
+    def __init__(self, sim: Simulator, **kwargs):
+        self.sim = sim
+        self.models = []
+        self.data: dict[int, list[tuple[int, bool]]] = defaultdict(list)
 
-    def plan(self, sim: Simulator, event: ShiftPlanEvent) -> list[Route]:
+        for _ in sim.containers:
+            model = SGDClassifier(
+                loss="log_loss",
+                penalty=None,
+                fit_intercept=False,
+            )
+
+            model.fit([0, 150], [0, 1])  # TODO make this data an argument
+            self.models.append(model)
+
+    def plan(self, event: ShiftPlanEvent) -> list[Route]:
+        # Step 1. Update the models with data observed since the last shift
+        # plan, and clear the data.
+        # TODO
+
+        # Step 2. Determine prizes, required containers, and solve the VRP.
+        # TODO
+
+        # Step 3. Return the route plan.
         # TODO
         return []
 
     def observe(self, event: Event):
-        if not isinstance(event, ServiceEvent):
-            # We're only interested in service events, since those tell us
-            # something about whether a container has overflowed.
-            return
-
-        # container = event.container
-        # num_arrivals = event.num_arrivals
-        # has_overflow = event.volume > container.capacity
-
-        # TODO update parameter estimate for container
+        if isinstance(event, ServiceEvent):
+            # Store the new data. We will update the model in one batch the
+            # next time we plan a shift.
+            container = event.container
+            num_arrivals = event.num_arrivals
+            has_overflow = event.volume > container.capacity
+            self.data[id(container)].append((num_arrivals, has_overflow))
