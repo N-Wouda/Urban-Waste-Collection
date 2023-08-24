6cdb406f47644f5cc0555f3625b71d0a7789d7cf
Niels Wouda <N-Wouda@users.noreply.github.com>
2023-08-24 14:40:07 +0200

    Add measure computing the average number of visited clusters (#51)
    * Add measure computing the average number of visited clusters

---
+++ tests/measures/test_avg_route_clusters.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+import pytest
+from numpy.random import default_rng
+from numpy.testing import assert_allclose
+
+from tests.helpers import MockStrategy
+from waste.classes import (
+    Database,
+    Event,
+    Route,
+    ShiftPlanEvent,
+    Simulator,
+)
+from waste.measures import avg_route_clusters
+
+
+@pytest.mark.parametrize(
+    ("visits", "expected"),
+    [
+        ([], 0.0),  # no routes: 0.0 average.
+        ([[1]], 1.0),  # one route: 1.0 average
+        ([[1], []], 0.5),  # two routes, one empty: 0.5 average
+        ([[1], [3, 4]], 1.0),  # 3 and 4 are in the same cluster: 1.0 average
+    ],
+)
+def test_for_several_routes(visits: list[list[int]], expected: float):
+    db = Database("tests/test.db", ":memory:")
+    sim = Simulator(
+        default_rng(0),
+        db.depot(),
+        db.distances(),
+        db.durations(),
+        db.containers(),
+        db.vehicles(),
+    )
+
+    now = datetime.now()
+    routes = [Route(plan, veh, now) for plan, veh in zip(visits, sim.vehicles)]
+    strategy = MockStrategy(sim, routes)
+
+    events: list[Event] = [ShiftPlanEvent(time=now)]
+    sim(db.store, strategy, events)
+
+    assert_allclose(db.compute(avg_route_clusters), expected)
---
+++ waste/measures/avg_route_clusters.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from waste.classes import Database
+
+
+def avg_route_clusters(db: Database, after: datetime) -> float:
+    """
+    Computes the average number of container clusters along routes. While the
+    number of stops (see ``avg_route_stops``) provides a similar measure, it
+    counts every container as a unique stop. In practice, many containers are
+    part of a cluster of containers, which might all be serviced at the same
+    time.
+    """
+    sql = """-- sql
+        SELECT AVG(num_clusters)
+        FROM (
+            SELECT routes.id_route, COUNT(DISTINCT c.cluster) AS num_clusters
+            FROM routes
+                LEFT JOIN service_events se 
+                    ON se.id_route = routes.id_route
+                LEFT JOIN containers c
+                    ON c.name = se.container
+            WHERE routes.start_time > ?
+            GROUP BY routes.id_route
+        );
+    """
+    row = db.write.execute(sql, [after]).fetchone()
+    return row[0] if row[0] is not None else 0.0
