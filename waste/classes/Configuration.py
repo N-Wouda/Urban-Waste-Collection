9a02ef507cc2ef4f09aa1022886afa1ccca89788
Niels Wouda <nielswouda@gmail.com>
2023-08-31 23:18:01 +0200

    Update break settings a little bit

--- waste/classes/Configuration.py
+++ waste/classes/Configuration.py
@@ -6,9 +6,9 @@ from datetime import time, timedelta
 class Configuration:
     BREAKS: tuple[tuple[time, timedelta], ...] = (
         # Coffee break starting at 10:00 and lasting 15 minutes, and
-        # a lunch break starting at 12:00, lasting 45 minutes.
+        # a lunch break starting at 12:00, lasting 30 minutes.
         (time(hour=10), timedelta(minutes=15)),
-        (time(hour=12), timedelta(minutes=45)),
+        (time(hour=12), timedelta(minutes=30)),
     )
     SHIFT_DURATION: timedelta = timedelta(hours=8)  # excl. breaks
     SHIFT_PLAN_TIME: time = time(hour=7)  # noqa: RUF009
