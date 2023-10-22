e834071e8a224f3c02ff0853f596e352f8ef87a0
Niels Wouda <nielswouda@gmail.com>
2023-10-22 16:47:23 +0200

    Add sensor data analysis notebook, update volume ranges

--- waste/classes/Configuration.py
+++ waste/classes/Configuration.py
@@ -13,4 +13,4 @@ class Configuration:
     SHIFT_DURATION: timedelta = timedelta(hours=8)  # excl. breaks
     SHIFT_PLAN_TIME: time = time(hour=7)  # noqa: RUF009
     TIME_PER_CONTAINER: timedelta = timedelta(minutes=3, seconds=30)
-    VOLUME_RANGE: tuple[float, float] = (30, 65)  # in liters
+    VOLUME_RANGE: tuple[float, float] = (10, 60)  # in liters
