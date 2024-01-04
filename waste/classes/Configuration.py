cb2c33353375ce54202743fe25081f8a148f2aa4
Niels Wouda <nielswouda@gmail.com>
2024-01-04 16:42:15 +0100

    Update configuration and sensordata notebook

--- waste/classes/Configuration.py
+++ waste/classes/Configuration.py
@@ -5,12 +5,12 @@ from datetime import time, timedelta
 @dataclass
 class Configuration:
     BREAKS: tuple[tuple[time, timedelta], ...] = (
-        # Coffee break starting at 10:00 and lasting 15 minutes, and
-        # a lunch break starting at 12:00, lasting 30 minutes.
-        (time(hour=10), timedelta(minutes=15)),
+        # Coffee break starting at 10:00 and lasting 30 minutes, and
+        # a lunch break starting at 12:00, also lasting 30 minutes.
+        (time(hour=10), timedelta(minutes=30)),
         (time(hour=12), timedelta(minutes=30)),
     )
-    SHIFT_DURATION: timedelta = timedelta(hours=8)  # excl. breaks
+    SHIFT_DURATION: timedelta = timedelta(hours=6)  # excl. breaks
     SHIFT_PLAN_TIME: time = time(hour=7)  # noqa: RUF009
     TIME_PER_CONTAINER: timedelta = timedelta(minutes=3, seconds=30)
     VOLUME_RANGE: tuple[float, float] = (10, 60)  # in liters
